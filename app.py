3b54f4ac361498c23b0e7e3993b1ec7ce671c96f
ANdIeCOOl <23f1002382@ds.study.iitm.ac.in>
2024-01-11 11:25:31 +0530

    Created the routes Using Flask

--- app.py
+++ app.py
@@ -1,1 +1,45 @@
-from flask import Flask
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/Admin<ID>")
+def AdminHomePage(ID):
+    return render_template("Admin.html")
+
+
+#Requests Page From Admin
+@app.route("/Admin<ID>/Requests")
+def AdminRequestsPage(ID):
+    return render_template("Requests.html")
+
+#Analytics Page From Admin
+@app.route("/Admin<ID>/Analytics")
+def AdminAnalyticsPage(ID):
+    return render_template("Analytics.html")
+
+
+# Users Page From Admin
+@app.route("/Admin<ID>/Users")
+def AdminUserPage(ID):
+    return render_template("Users.html")
+
+
+#Sections Page From Admin
+@app.route("/Admin<ID>/Sections")
+def AdminSectionsPage(ID):
+    return render_template("Sections.html")
+
+
+#Books Page From Admin
+@app.route("/Admin<ID>/Books")
+def AdminBooksPage(ID):
+    return render_template("Books.html")
+
+
+if (__name__ == "__main__"):
+    app.run()
